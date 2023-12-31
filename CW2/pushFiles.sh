81bc51a9677253ee9e04eb007a86a504692c9f49
Oguz Tec <69775148+oguztecirlioglu@users.noreply.github.com>
2023-12-31 00:49:28 +0000

    47/70, parser somewhat works.

--- CW2/pushFiles.sh
+++ CW2/pushFiles.sh
@@ -1,1 +1,9 @@
-cp cool.y ~/cw2
+cp cool.y pa2-grading.pl ~/cw2
+echo "*** Files pushed successfully."
+
+cd ~/cw2
+make parser
+echo "*** Recompiled parser successfully."
+
+echo "*** Running grading script:"
+perl pa2-grading.pl
